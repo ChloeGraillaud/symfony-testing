47e41863a2638cf38d201e6bb19343574d1f5a85
ChloeGraillaud <cgraillaud@laposte.net>
2024-10-21 10:23:48 +0200

    Tests

---
+++ my_project_directory/src/Entity/Product.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $libellé = null;
+
+    #[ORM\Column]
+    private ?float $prix = null;
+
+    #[ORM\Column]
+    private ?int $quantité = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibellé(): ?string
+    {
+        return $this->libellé;
+    }
+
+    public function setLibellé(string $libellé): static
+    {
+        $this->libellé = $libellé;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): static
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getQuantité(): ?int
+    {
+        return $this->quantité;
+    }
+
+    public function setQuantité(int $quantité): static
+    {
+        $this->quantité = $quantité;
+
+        return $this;
+    }
+}
